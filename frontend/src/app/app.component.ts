27f46cf31cd3c8ed5d2030a992e1f51373a3c477
JacobFriedges <122567961+JacobFriedges@users.noreply.github.com>
2025-02-24 05:30:36 -0600

    Angular 19 Template Implemented
    Added the working template files. Also started experimenting with changing the logo name and creating sidebar buttons for all jobs and daily board. Pages not created for those yet.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// Angular import
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+
+// project import
+import { SpinnerComponent } from './theme/shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SpinnerComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+
+  title = 'datta-able';
+
+  // life cycle hook
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
 }
