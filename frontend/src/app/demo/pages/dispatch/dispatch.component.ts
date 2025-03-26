9f2e6e001a22f009a0bfb6a8f282240f093fe178
JacobFriedges <122567961+JacobFriedges@users.noreply.github.com>
2025-03-26 04:12:32 -0500

    Added tables for assignments page
    Added tables for assignments page. Also the date automatically converts to a more readable format.

--- frontend/src/app/demo/pages/dispatch/dispatch.component.ts
+++ frontend/src/app/demo/pages/dispatch/dispatch.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DispatchDialogComponent } from './dispatch-dialog/dispatch-dialog.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dispatch',
   templateUrl: './dispatch.component.html',
-  styleUrls: ['./dispatch.component.scss']
+  styleUrls: ['./dispatch.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  providers: [DatePipe]
 })
 export class DispatchComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(private datePipe: DatePipe, public dialog: MatDialog) {}
 
   openDialog(): void {
     this.dialog.open(DispatchDialogComponent, {
       width: '400px'
     });
   }
+
+  toggleAllSelection(event: any) {
+    const checked = event.target.checked;
+    this.assignments.forEach(customer => customer.selected = checked);
+  }
+
+  getFormattedDate(date: string): string {
+    return this.datePipe.transform(date, 'MMMM d, yyyy') || '';
+  }
+
+  assignments = [
+    { job: 'HW72', driver: 'John Doe', truck_type: 'Semi', date: '2025-03-13', selected: false },
+    { job: 'I-32', driver: 'Jane Doe', truck_type: 'Belly Dump', date: '2025-06-25', selected: false }
+  ];
 }
